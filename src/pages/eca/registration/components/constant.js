25586963c6ab28364ab88ff0d02cb4322f32e63e
Cyrus Ngan <cyrusncy@gmail.com>
2025-07-30 22:34:03 +0800

    first commit for eca

---
+++ src/pages/eca/registration/components/constant.js
@@ -0,0 +1,35 @@
+export const WEEKDAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+export const MODE_TYPES = {
+  exact: {
+    cname: '恆常',
+    example: '如每逢星期三'
+  },
+  regular: {
+    cname: '定期',
+    example: '如每月/每雙月/每季等'
+  },
+  date: {
+    cname: '指定日子或時段',
+    example: '2024年12月,\n 2025年3月5、7日'
+  }
+}
+export const ACTIVITY_TYPES = {
+  course: {
+    cname: '興趣班'
+  },
+  workshop: {
+    cname: '工作坊'
+  },
+  training: {
+    cname: '訓練'
+  },
+  competition: {
+    cname: '比賽'
+  },
+  performance: {
+    cname: '表演'
+  },
+  others: {
+    cname: '其他'
+  }
+}
