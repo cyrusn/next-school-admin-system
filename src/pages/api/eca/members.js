7df45ba82e437230d6821e37e569175d135736da
Cyrus Ngan <cyrusncy@gmail.com>
2025-08-07 21:26:09 +0800

    bug fix for eca member modules

--- src/pages/api/eca/members.js
+++ src/pages/api/eca/members.js
@@ -3,11 +3,13 @@ import { getSession } from 'next-auth/react'
 import {
   appendRows,
   getSheetData,
+  batchClearData,
   batchGetSheetDataByColumn,
   batchGetSheetDataByRow,
   batchUpdateSpreadsheet
 } from '@/utils/googleSheet'
 const spreadsheetId = process.env.ECA_EVALUATION_SSID
+import { getTimestamp } from '@/lib/helper'
 
 export const putHandler = async (req, res) => {
   try {
@@ -40,12 +42,50 @@ export const putHandler = async (req, res) => {
     res.status(500).json({ error: error.message })
   }
 }
+const uid = function (clubId) {
+  return (
+    Date.now().toString(36) +
+    clubId.toString(36) +
+    Math.random().toString(36).substring(2)
+  )
+}
 
 export const postHandler = async (req, res) => {
   try {
-    const { rows } = req.body
+    const { club, students } = req.body
+    const {
+      id,
+      category,
+      cname: information,
+      pic,
+      associates,
+      admininstrators
+    } = club
+    const timestamp = getTimestamp()
+
+    const rows = students.map(({ regno, classcode, classno, cname, ename }) => {
+      const classcodeAndNo = `${classcode}${String(classno).padStart(2, 0)}`
+      const studentName = cname || ename
+
+      return [
+        timestamp,
+        uid(id),
+        id,
+        category,
+        information,
+        regno,
+        classcodeAndNo,
+        studentName,
+        '',
+        '',
+        '',
+        pic,
+        associates,
+        admininstrators
+      ]
+    })
     const response = await appendRows(spreadsheetId, 'record!A1:A1', rows)
-    res.status(200).json({ response })
+    res.status(200).json(response)
   } catch (error) {
     console.error('Error accessing Spreadsheet:', error)
     res.status(500).json({ error: error.message })
@@ -95,7 +135,20 @@ export const getHandler = async (req, res) => {
   }
 }
 
-export const deleteHandler = async (req, res) => {}
+export const deleteHandler = async (req, res) => {
+  try {
+    const { ranges } = req.body
+
+    const participantsDataResponse = await batchClearData(spreadsheetId, ranges)
+    const { clearedRanges: clearedParticipantRanges } = participantsDataResponse
+
+    res.status(200).json({ clearedParticipantRanges })
+  } catch (error) {
+    console.error('Error accessing Spreadsheet:', error)
+    res.status(500).json({ error: error.message })
+  }
+}
+
 export default async function handler(req, res) {
   const { method } = req
   const body = { ...req.body }
@@ -118,6 +171,7 @@ export default async function handler(req, res) {
       await putHandler(req, res)
       break
     case 'DELETE':
+      req.body = body
       await deleteHandler(req, res)
       break
     default:
