78ecf959db02922e23151df552561dbf5da3217f
Cyrus Ngan <cyrusncy@gmail.com>
2025-08-20 22:19:04 +0800

    use grid for photos, tag 1.0.10

--- src/pages/photo/index.js
+++ src/pages/photo/index.js
@@ -42,76 +42,81 @@ export default function StudentPhoto() {
 
   return (
     <>
-      <h1 className='title has-text-centered'>Student Photo</h1>
-      <div className='field is-horizontal'>
-        <div className='field-label'>
-          <label className='label'>class</label>
-        </div>
-        <div className='field-body'>
-          <div className='field'>
-            <div className='control'>
-              <div className='select is-fullwidth'>
-                <select onChange={handleChange}>
-                  <option value={''}>Select class</option>
-                  {classcodes.map((classcode) => {
-                    return (
-                      <option key={classcode} value={classcode}>
-                        {classcode}
-                      </option>
-                    )
-                  })}
-                </select>
+      <div className='not-print'>
+        <h1 className='title has-text-centered'>Student Photo</h1>
+        <div className='field is-horizontal'>
+          <div className='field-label'>
+            <label className='label'>class</label>
+          </div>
+          <div className='field-body'>
+            <div className='field'>
+              <div className='control'>
+                <div className='select is-fullwidth'>
+                  <select onChange={handleChange}>
+                    <option value={''}>Select class</option>
+                    {classcodes.map((classcode) => {
+                      return (
+                        <option key={classcode} value={classcode}>
+                          {classcode}
+                        </option>
+                      )
+                    })}
+                  </select>
+                  <div className='help is-info'>
+                    If you want to print the photos, remember to adjust the
+                    margins and scaling settings for optimal viewing quality. e.g. set the scale to 50%
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {filter ? (
-        <div className='columns is-multiline is-desktop'>
-          {students
-            .filter((s) => s.classcode == filter)
-            .map((student) => {
-              const { regno, name, cname, sex, classcode, classno } = student
-              const classcodeAndNo = `${classcode}${String(classno).padStart(2, 0)}`
-              const found = files?.find(
-                (file) => file.name.split('.')[0] == `lp${regno}`
-              )
-              return (
-                <div
-                  className='column is-one-quarter-desktop has-text-centered'
-                  key={regno}
-                >
-                  <div className='box'>
-                    <div className='is-flex is-justify-content-center'>
-                      {found && (
-                        <figure>
-                          <Image
-                            alt={regno}
-                            src={found.thumbnailLink}
-                            width='160'
-                            height='320'
-                          />
-                        </figure>
-                      )}
-                    </div>
-                    {cname && <p>{cname}</p>}
-                    <p>{name}</p>
-                    <div className='tags is-justify-content-center'>
-                      <span className='tag is-dark'>{classcodeAndNo}</span>
-                      <span className='tag is-success'>{regno}</span>
-                      <span
-                        className={`tag ${sex == 'M' ? 'is-info' : 'is-danger'}`}
-                      >
-                        {sex}
-                      </span>
+
+      {filter && (
+        <div className='section'>
+          <h1 className='title has-text-centered'>{filter}</h1>
+          <div className='grid is-col-min-8'>
+            {students
+              .filter((s) => s.classcode == filter)
+              .map((student) => {
+                const { regno, name, cname, sex, classcode, classno } = student
+                const classcodeAndNo = `${classcode}${String(classno).padStart(2, 0)}`
+                const found = files?.find(
+                  (file) => file.name.split('.')[0] == `lp${regno}`
+                )
+                return (
+                  <div className='cell has-text-centered' key={regno}>
+                    <div className='box'>
+                      <div className='is-flex is-justify-content-center'>
+                        {found && (
+                          <figure>
+                            <Image
+                              alt={regno}
+                              src={found.thumbnailLink}
+                              width='160'
+                              height='320'
+                            />
+                          </figure>
+                        )}
+                      </div>
+                      {cname && <p>{cname}</p>}
+                      <p>{name}</p>
+                      <div className='tags is-justify-content-center'>
+                        <span className='tag is-dark'>{classcodeAndNo}</span>
+                        <span className='tag is-success'>{regno}</span>
+                        <span
+                          className={`tag ${sex == 'M' ? 'is-info' : 'is-danger'}`}
+                        >
+                          {sex}
+                        </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              )
-            })}
+                )
+              })}
+          </div>
         </div>
-      ) : (
-        <></>
       )}
     </>
   )
