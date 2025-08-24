272018816c027d782452341f377a7878eafd5029
Cyrus Ngan <cyrusncy@gmail.com>
2025-08-24 22:30:26 +0800

    rename file

--- src/pages/ipad/nav.js
+++ src/pages/ipad/nav.js
@@ -2,10 +2,9 @@ import Nav from '@/components/nav'
 import { useSession } from 'next-auth/react'
 
 export default function IpadNav() {
-  const { data: session, status } = useSession()
   const paths = [
     { href: '/ipad/form', label: 'Form' },
-    { href: '/ipad/result', label: 'Result' }
+    { href: '/ipad/record', label: 'Record' }
   ]
 
   return <Nav paths={paths} title='iPad Application Records' />
