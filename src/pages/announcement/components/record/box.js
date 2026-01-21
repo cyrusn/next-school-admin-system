7a5185b5dcf144eb8aa5df26fc7ac8959fc1cb62
Cyrus Ngan <cyrusncy@gmail.com>
2026-01-21 08:36:03 +0800

    Improve print functionality for announcement box

--- src/pages/announcement/components/record/box.js
+++ src/pages/announcement/components/record/box.js
@@ -10,16 +10,35 @@ const Box = ({ date, events, onDelete }) => {
   function handlePrint(id) {
     const html = document
       .getElementById(id)
-      .innerHTML.replace(/\<button.*\<\/button\>/gm, '')
+      .innerHTML.replace(/\<button.*?\<\/button\>/gm, '')
       .replace(/is-info/gm, '')
 
     const printWindow = window.open('', 'PRINT')
     printWindow.document.write(`
           <!DOCTYPE html>
+          <html lang="zh-HK">
             <head>
+              <meta charset="utf-8" >
               <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
               <title>${id}</title>
               <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+              <style>
+                body {
+                  font-family: sans-serif;
+                }
+                @media print {
+                  .message-header {
+                    background-color: #fff !important;
+                    color: #000 !important;
+                    border: 1px solid #000 !important;
+                  }
+                  .message-body {
+                    color: #000 !important;
+                    border: 1px solid #000 !important;
+                    border-top: none !important;
+                  }
+                }
+              </style>
             </head>
             <body>
               <div class='container is-max-desktop'>
@@ -30,6 +49,7 @@ const Box = ({ date, events, onDelete }) => {
             </body>
           </html>
         `)
+    printWindow.document.close()
   }
 
   return (
